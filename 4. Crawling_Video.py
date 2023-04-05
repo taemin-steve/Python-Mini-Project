1a96e223a0db336680f3dcdc85a865b0f6ba46a5
EHmin <okchrisok@naver.com>
2023-04-05 11:24:08 +0900

    all most

--- 4. Crawling_Video.py
+++ 4. Crawling_Video.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import urllib.request
 import time
+
+# tip CTRL + Shift + C 
 # get keyword
 query = '코로나'
-
-urllib.request.urlretrieve("https://www.youtube.com/embed/ZFUnG41xJOY", 'video.mp4')
 
 #access to url by chrome driver
 url = 'https://www.google.com/'
@@ -15,22 +15,35 @@ driver.get(url)
 time.sleep(5)
 
 
+
 #find search box, and put keyword 
-search_box = driver.find_element(By.CSS_SELECTOR, "textarea#APjFqb")
+# search_box = driver.find_element(By.CSS_SELECTOR, "textarea#APjFqb")
+
+# search_box = driver.find_element(By.CSS_SELECTOR, "textarea#APjFqb")
+# search_box = driver.find_element(By.ID, "APjFqb")
+search_box = driver.find_element(By.CLASS_NAME, "gLFyf")
+# search_box = driver.find_element(By.NAME, 'q')
+# search_box = driver.find_element(By.CSS_SELECTOR, 'textarea.gLFyf')
+# search_box = driver.find_element(By.XPATH, '//textarea[@id="APjFqb"]')
+
+
 search_box.send_keys(query)
 search_box.send_keys(Keys.RETURN)
 time.sleep(5)
-
 
 #click news tab by using xPath
 driver.find_element(By.XPATH,'//*[@id="hdtb-msb"]/div[1]/div/div[4]/a').click()
 time.sleep(15)
 
-video_links  = driver.find_elements(By.TAG_NAME,"a")
 
-# for link in video_links:
-#     href = link.get_attribute('href')
-#     if href and href.startswith('https://www.youtube.com/watch'):
-#         urllib.request.urlretrieve(href, 'video.mp4')
+
+video_links  = driver.find_elements(By.CLASS_NAME,"VYkpsb")
+
+j = 0
+
+for link in video_links:
+    src = link.get_attribute('data-url')
+    if (src != None):
+        urllib.request.urlretrieve(src, "video.mp4")
         
         
