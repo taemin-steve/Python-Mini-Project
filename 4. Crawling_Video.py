dc696e5fe2022daf6fc8d4e4c17911a9e3bef87d
EHmin <okchrisok@naver.com>
2023-04-05 14:27:12 +0900

    complete

--- 4. Crawling_Video.py
+++ 4. Crawling_Video.py
@@ -15,35 +15,24 @@ driver.get(url)
 time.sleep(5)
 
 
-
-#find search box, and put keyword 
-# search_box = driver.find_element(By.CSS_SELECTOR, "textarea#APjFqb")
-
-# search_box = driver.find_element(By.CSS_SELECTOR, "textarea#APjFqb")
-# search_box = driver.find_element(By.ID, "APjFqb")
 search_box = driver.find_element(By.CLASS_NAME, "gLFyf")
-# search_box = driver.find_element(By.NAME, 'q')
-# search_box = driver.find_element(By.CSS_SELECTOR, 'textarea.gLFyf')
-# search_box = driver.find_element(By.XPATH, '//textarea[@id="APjFqb"]')
-
-
 search_box.send_keys(query)
 search_box.send_keys(Keys.RETURN)
 time.sleep(5)
 
 #click news tab by using xPath
 driver.find_element(By.XPATH,'//*[@id="hdtb-msb"]/div[1]/div/div[4]/a').click()
-time.sleep(15)
+time.sleep(5)
 
 
-
-video_links  = driver.find_elements(By.CLASS_NAME,"VYkpsb")
+video_links = driver.find_elements(By.CLASS_NAME,"VYkpsb")
 
 j = 0
 
 for link in video_links:
     src = link.get_attribute('data-url')
     if (src != None):
-        urllib.request.urlretrieve(src, "video.mp4")
+        urllib.request.urlretrieve(src, "video" + str(j) + ".mp4")
+        j+= 1
         
         
